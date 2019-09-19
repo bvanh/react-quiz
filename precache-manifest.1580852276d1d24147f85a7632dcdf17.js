42e65c2b15c21cc991e85df332ce1af9ecefe880
bvanh <48560384+bvanh@users.noreply.github.com>
2019-09-19 13:12:10 +0700

    Updates

---
+++ precache-manifest.1580852276d1d24147f85a7632dcdf17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4adf4c97c682d8f25886c5f48ddfc0ea",
+    "url": "/react-quiz/index.html"
+  },
+  {
+    "revision": "a86594f75e2fb27c21f0",
+    "url": "/react-quiz/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "7872b745a03a7af0cdf0",
+    "url": "/react-quiz/static/css/main.0d05beb7.chunk.css"
+  },
+  {
+    "revision": "a86594f75e2fb27c21f0",
+    "url": "/react-quiz/static/js/2.d85c536c.chunk.js"
+  },
+  {
+    "revision": "7872b745a03a7af0cdf0",
+    "url": "/react-quiz/static/js/main.733d2c62.chunk.js"
+  },
+  {
+    "revision": "fe23e9de900c2d0a3e56",
+    "url": "/react-quiz/static/js/runtime~main.c5b7b6c1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-quiz/precache-manifest.bb0fdfb3bb926f5bdcd97e901ecdd16a.js"
+  "/react-quiz/precache-manifest.1580852276d1d24147f85a7632dcdf17.js"
 );
 
 self.addEventListener('message', (event) => {
